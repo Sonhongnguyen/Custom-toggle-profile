74b113244123bb2aad19d42ef840b8d6432ef7e1
Marcus <son6101999@gmail.com>
2026-02-06 17:39:44 +0700

    Update layout

--- extensions/customer-toggle-tag/src/OrderStatusBlock.jsx
+++ extensions/customer-toggle-tag/src/OrderStatusBlock.jsx
@@ -1,19 +1,30 @@
 import '@shopify/ui-extensions/preact';
 import {render} from "preact";
+import {useState} from 'preact/hooks';
 
 export default async () => {
   render(<Extension />, document.body)
 }
 
 function Extension() {
+  const checboxLabel = shopify.settings?.value.label_checkbox || "Nhận email khuyến mãi";
+  const actionDescription = shopify.settings?.value.action_description || "Lam gi co gi bat ngo dau :D";
+  const [checked, setChecked] = useState(false);
+  const dataJetId = shopify.settings?.value.datajet_id_action || "";
   return (
     <s-banner>
-      <s-text>
-        {shopify.i18n.translate("earnPoints")}
-      </s-text>
-      <s-checkbox
-          label="Nhận email khuyến mãi"
+        <s-checkbox
+          label={checboxLabel}
+          checked={checked}
+          onChange={(event) => {
+            setChecked(event.target.checked);
+          }}
         />
+        {checked && (
+          <s-text>
+            {actionDescription}
+          </s-text>
+        )}
     </s-banner>
   );
 }
